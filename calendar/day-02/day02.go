87099b0e4dba26c370c2bfd95f9c752c2eb72785
Jonathon Ramey <jonathon627@gmail.com>
2021-12-01 22:08:42 -0800

    init day2

---
+++ calendar/day-02/day02.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"advent-of-go-2021/utils/files"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type Cord struct {
+	X int64
+	Y int64
+}
+
+func main() {
+	input := files.ReadFile(2, "\n")
+	fmt.Printf("Part 1: %d\n", solveDay2(input))
+}
+
+func solveDay2(input []string) int64 {
+	x := int64(0)
+	y := int64(0)
+	aim := int64(0)
+	for i := range input {
+		split := strings.SplitAfter(input[i], " ")
+		dist, _ := strconv.ParseInt(split[1], 10, 0)
+		dir := strings.TrimSpace(split[0])
+		switch dir {
+		case "forward":
+			x += dist
+			y += aim * dist
+		case "up":
+			aim -= dist
+		case "down":
+			aim += dist
+		default:
+			continue
+		}
+	}
+	fmt.Println(x)
+	fmt.Println(y)
+	return x * y
+}
+
